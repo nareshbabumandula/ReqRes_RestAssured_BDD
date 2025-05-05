f1c433951401971ab62f66f79a16b4391bdca122
[nareshbabumandula] <naresh223@gmail.com>
2025-05-05 17:07:25 +0530

    Test Commit

--- src/test/java/stepdefinitions/CRUDStepDefs.java
+++ src/test/java/stepdefinitions/CRUDStepDefs.java
@@ -82,7 +82,6 @@ public class CRUDStepDefs extends BaseClass{
 
 		JsonPath jsonPath = response.jsonPath();
 		
-		// Retrieve ID
 		int IDValue = jsonPath.getInt("id");
 		String tokenValue = jsonPath.getString("token"); 
 
